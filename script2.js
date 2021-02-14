53307acc25c66ecd46db80311f24fbaf10e6c140
Doug Daniels <daniels.douglas@gmail.com>
2021-02-14 17:37:56 +0000

    stuff

--- script2.js
+++ script2.js
@@ -1,5 +1,5 @@
-var msg = "HELLO ERIN WORLD! " + (2+3)
+var msg = "HELLO ERIN WORLD! " + (2+5)
 console.log(msg)
 
 // Remove the "//" comment from the line below and see an alert box show up when the page loads.
-// alert(msg)
+alert(msg)
