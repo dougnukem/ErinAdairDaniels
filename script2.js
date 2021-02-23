4fe35bb40394da8235649b0c14de4009ded23e98
Doug Daniels <daniels.douglas@gmail.com>
2021-02-23 19:40:38 +0000

    added source links

--- script2.js
+++ script2.js
@@ -1,5 +1,5 @@
-var msg = "HELLO ERIN WORLD! " + (2+5)
+var msg = "HELLO ERIN WORLD! " + (2+11)
 console.log(msg)
 
 // Remove the "//" comment from the line below and see an alert box show up when the page loads.
-alert(msg)
+// alert(msg)
